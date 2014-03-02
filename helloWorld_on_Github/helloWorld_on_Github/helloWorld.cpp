517336adb29ba6ec5108ad168bc8658cb84465fa
nickehrlich <nick.ehrlich@live.com>
2014-03-02 16:44:43 -0600

    rebuilt solution

--- helloWorld_on_Github/helloWorld_on_Github/helloWorld.cpp
+++ helloWorld_on_Github/helloWorld_on_Github/helloWorld.cpp
@@ -6,7 +6,6 @@ int main()
 {
 	cout << " HELLO...DOCTOR...NAME...CONTINUE...YESTERDAY...TOMORROW.\n";
 	cout << " Good morning, Dr Chandra, this is Hal. I'm ready for my first lesson.\n\n";
-	// cin.get(); not needed. Use "DEBUG > Start Without Debugging (cntrl F5)  
-	//adding comment to touch
+	//cin.get(); not used. Select DEBUG > Start Without Debuging (cntrl F5) to have system wait to close console. 
 	return 0;
 }
