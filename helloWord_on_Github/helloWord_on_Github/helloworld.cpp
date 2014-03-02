1e37ed295d01c9e792ff4185a04dbdb18cb71a3a
nickehrlich <nick.ehrlich@live.com>
2014-03-02 16:26:14 -0600

    third try at cleanup

---
+++ helloWord_on_Github/helloWord_on_Github/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Hello World
+int main()
+{
+	cout << " HELLO...DOCTOR...NAME...CONTINUE...YESTERDAY...TOMORROW.\n";
+	cout << " Good morning, Dr Chandra, this is Hal. I'm ready for my first lesson.\n\n";
+	// cin.get(); not needed. Use "DEBUG > Start Without Debugging (cntrl F5)  
+	//adding comment to touch
+	return 0;
+}
